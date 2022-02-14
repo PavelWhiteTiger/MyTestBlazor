dd57025b05e8802f088035e2890c1a7fa32c8c6e
PavelWhiteTiger <71945087+PavelWhiteTiger@users.noreply.github.com>
2022-02-14 18:58:27 +0500

    1 Lesson END

---
+++ MyTestBlazor/Models/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTestBlazor.Models;
+
+public class Game
+{
+    public string Name { get; set; }
+    public string Genre { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
