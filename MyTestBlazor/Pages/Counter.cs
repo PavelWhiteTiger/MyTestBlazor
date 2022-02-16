7ec6857062160babeb0acd6227ea563c2c972feb
PavelWhiteTiger <71945087+PavelWhiteTiger@users.noreply.github.com>
2022-02-16 18:51:40 +0500

    6 Lesson END

---
+++ MyTestBlazor/Pages/Counter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyTestBlazor.Services;
+
+namespace MyTestBlazor.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+    [Inject] private SingletontServices _singletontServices { get; set; }
+    [Inject] private TransientServices _transientServices { get; set; } 
+
+    private void IncrementCount()
+    {
+        currentCount++;
+        _transientServices.Value++;
+        _singletontServices.Value++;
+    }
+}
