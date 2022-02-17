a0363905aacb798f98d6f2c43595da8edd3574d2
PavelWhiteTiger <71945087+PavelWhiteTiger@users.noreply.github.com>
2022-02-17 19:53:22 +0500

    9 Lesson END

---
+++ MyBlazor2/Models/GameModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBlazor2.Models;
+
+public class GameModel
+{
+    public int Id { get; set; }
+    public string Genre{ get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public DateTime DateRelease { get; set; }
+}
