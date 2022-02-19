8c3dac8a25b479478c824cab0d510db317b6d26c
PavelWhiteTiger <71945087+PavelWhiteTiger@users.noreply.github.com>
2022-02-19 23:01:46 +0500

    11 Lesson 41 min

---
+++ MyBlazor2/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazor2.Models;
+
+public class Genre
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    
+}
